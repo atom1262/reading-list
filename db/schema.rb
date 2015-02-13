1fca482390fc57cb4ef0e1a91fe6b6bfeb7e4a79
Adam Patterson <adam.patterson0012@gmail.com>
2015-02-12 21:41:12 -0500

    adds finished_on column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213022727) do
+ActiveRecord::Schema.define(version: 20150213023249) do
 
   create_table "books", force: true do |t|
     t.string   "title",       null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150213022727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rating"
+    t.date     "finished_on"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@ Book.create!([{
   title: "The Hobbit",
   author: "JRR Tolkien",
   description: "A hobbit goes in search of a ring...",
-  amazon_id: "054792822X"
+  amazon_id: "054792822X",
+  rating: 5,
+  finished_on: 10.days.ago,
 
 },
 {
@@ -14,7 +16,9 @@ Book.create!([{
   title: "The Catcher in the Rye",
   author: "J.D. Salinger",
   description: "A 16 year old hates everything...",
-  amazon_id: "0316769487"
+  amazon_id: "0316769487",
+  rating: 3,
+  finished_on: 25.days.ago,
 
 }])
 
