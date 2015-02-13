8ce722eeb16b8c9ecccb672f782a32eb5ac737a9
Adam Patterson <adam.patterson0012@gmail.com>
2015-02-12 21:32:06 -0500

    adds rating column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216021537) do
+ActiveRecord::Schema.define(version: 20150213022727) do
 
   create_table "books", force: true do |t|
     t.string   "title",       null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141216021537) do
     t.string   "amazon_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
 end
