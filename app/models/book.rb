000692c084a716629aae2491d0d3fcdbb228f19f
Adam Patterson <adam.patterson0012@gmail.com>
2015-02-13 19:23:59 -0500

    adds keywords column to book table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
 
-  scope :recently_finished, ->{ where('finished_on > ?', 15.days.ago)}
+  scope :recently_finished, ->{ where('finished_on > ?', 15.days.ago) }
+  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
 
   def finished?
     finished_on.present?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213023249) do
+ActiveRecord::Schema.define(version: 20150214002217) do
 
   create_table "books", force: true do |t|
     t.string   "title",       null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150213023249) do
     t.datetime "updated_at"
     t.integer  "rating"
     t.date     "finished_on"
+    t.text     "keywords"
   end
 
 end
